7a94580a4ff542675b1858fabc247a69e5d655da
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-04 14:26:55 -0400

    secure password practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -14,11 +15,18 @@ class ApplicationController < Sinatra::Base
 
   get "/signup" do
     erb :signup
+
   end
 
   post "/signup" do
     #your code here
+    user = User.new(:username => params[:username], :password => params[:password])
 
+    if user.save && user.username != ""
+        redirect "/login"
+    else
+        redirect "/failure"
+    end
   end
 
   get '/account' do
@@ -32,7 +40,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/account"
+    else
+        redirect "/failure"
+    end
   end
 
   get "/failure" do
